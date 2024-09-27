2fd7a5ed3f5486141170e8ee24dddf40ea6d430d
Ashmit111 <ashmitsingh862@gmail.com>
2024-09-27 20:50:24 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, {useState,useEffect} from 'react';
 import {useDispatch} from 'react-redux'
 import './App.css'
 import authService from './appwrite/auth';
-import { login,logout } from './store/authslice';
+import { login,logout } from './store/authSlice';
 import {Header,Footer} from './components/index'
 import {Outlet} from 'react-router-dom'
 
